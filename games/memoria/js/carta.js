7fa705e7de310a9cfae95d607942c5555a954547
Albert Diaz <diazalbert45@gmail.com>
2020-07-08 18:46:58 -0400

    mejora 2 memoria

--- games/memoria/js/carta.js
+++ games/memoria/js/carta.js
@@ -3,21 +3,17 @@ const carta = {
 		const cantidadCartas = cartas.todasCartas.length;
 		const ancho = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
 		const alto = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
-		const anchoActual = ancho - 10 - (cantidadCartas*20);
-		const altoActual = alto - 100;
-		const areaTablero = anchoActual*altoActual;
+		const areaTablero = (ancho-10)*(alto-85);
 		const areaCarta = areaTablero/cantidadCartas;
-		const altoAncho = Math.sqrt(areaCarta);
-		const fontSize = altoAncho - 20;
+		const altoAncho = Math.sqrt(areaCarta)-20;
 
 		let tablero = document.getElementById('tablero');
-		tablero.style.gridTemplateColumns = `repeat(auto-fit, ${altoAncho}px)`;
-		tablero.style.gridTemplateRows = `repeat(auto-fit, ${altoAncho}px)`;
+		(ancho <= 480) ?tablero.style.height = `calc(${alto}px - 65px)` :tablero.style.height = `calc(${alto}px - 85px)`;
 		tablero.innerHTML = '';
 
 		cartas.todasCartas.map(carta => {
 			tablero.insertAdjacentHTML('beforeend', 
-				`<div class="carta" style="height:${altoAncho}px; width:${altoAncho}px; font-size:${fontSize}px;">
+				`<div class="carta" style="height:${altoAncho}px; width:${altoAncho}px;">
 				  <i class="fa ${carta}" style="height:${altoAncho}px; width:${altoAncho}px;"></i>
 				  <span class="back" style="height:${altoAncho}px; width:${altoAncho}px;"></span>
 				</div>`);
